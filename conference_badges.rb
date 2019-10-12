451e3db6dad5d396d627382450aca25906a318b9
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-10-12 18:03:14 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,38 @@
+require 'pry'
+
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(names)
+  empty_array = []
+  names.each do |name|
+    empty_array.push(badge_maker(name))
+  end
+  return empty_array
+end
+
+def assign_rooms(speakers)
+  count = 0 
+  rooms = [1,2,3,4,5,6,7]
+  assigned_room = []
+  speakers.each do |speaker|
+   assigned_room.push("Hello, #{speaker}! You'll be assigned to room #{rooms[count]}!")
+    count += 1 
+  end
+  assigned_room
+  # binding.pry
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).collect do |attendee_badge|
+      puts "#{attendee_badge}"
+    end
+    assign_rooms(attendees).collect do |attendee_room|
+      puts "#{attendee_room}"
+    end
+      # binding.pry
+end
+    
