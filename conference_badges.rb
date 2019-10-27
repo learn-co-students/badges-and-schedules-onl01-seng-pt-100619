78c8b43d863e0662a5b11fe01a087c97fdf209f7
Jay W <januarydwilson@gmail.com>
2019-10-27 23:24:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+   count = 0
+   msg = "Hello, my name is #{name}."
+   
+   for name in array
+       array[count] = msg
+	   count += 1
+   end
+end
+
+def assign_rooms(array)
+   ct = 0
+   rooms = [1, 2, 3, 4, 5, 6, 7]
+   
+   for ppl in array
+      print "Hello, + " array[ct] + "! You'll be assigned to room + " rooms[ct] + "."
+   end
+end
